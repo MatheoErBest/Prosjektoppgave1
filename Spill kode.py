e8b29978c43626d58d72aef7fe9d69288db16aa1
“MatheoErBest” <matheo.roen@gmail.com>
2023-10-19 12:50:19 +0200

    skjelletett på menyen integret i main loopen

--- Spill kode.py
+++ Spill kode.py
@@ -2,9 +2,12 @@ import pygame, sys, os
 
 #sprite
 
+    #spiller
 class Player(pygame.sprite.Sprite):
     def __init__():
         super().__init__()
+
+    #meny
 
 
 #display
@@ -27,8 +30,14 @@ red = (255, 0, 0)
 
 #funksjoner
 
+def drawText(text, font, color, x, y):
+    text_surface = font.render(text, True, color)
+    text_rect = text_surface.get_rect()
+    text_rect.center = (x, y)
+    screen.blit(text_surface, text_rect)
+
 def screenUpdate():
-    screen.fill((blue))
+    screen.fill((black))
     pygame.display.flip()
     pygame.display.update()
     clock.tick(60)
@@ -40,6 +49,17 @@ while Running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             Running = False
+
+    #tegne menyen
+    screen.fill((0, 0, 0))  # Sett bakgrunnsfargen
+    menu_font = pygame.font.Font(None, 36)
+
+    drawText("Main Menu", menu_font, (255, 255, 255), screenX // 2, 100)
+    drawText("Start Game", menu_font, (255, 255, 255), screenX // 2, 200)
+    drawText("Options", menu_font, (255, 255, 255), screenX // 2, 250)
+    drawText("Exit", menu_font, (255, 255, 255), screenX // 2, 300)
+
+
     screenUpdate()
 
 pygame.quit()
